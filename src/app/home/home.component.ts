7356743aac805ae6b81d5827d50a703d37088184
Srikanth Kakumanu <srikanth.kakumanu@smartdocsnow.com>
2019-11-30 21:29:35 +0530

    home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -25,7 +25,7 @@ const ELEMENT_DATA: PeriodicElement[] = [
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.scss']
 })
 
 export class HomeComponent implements OnInit {
