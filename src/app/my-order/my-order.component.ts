9cd6f5406716b5b3ec3fe705c7899efec9ea139f
Srikanth Kakumanu <srikanth.kakumanu@smartdocsnow.com>
2019-12-18 20:59:30 +0530

    changes for buttons

--- src/app/my-order/my-order.component.ts
+++ src/app/my-order/my-order.component.ts
@@ -1,7 +1,10 @@
-import { Component, OnInit, Pipe } from '@angular/core';
+import { Component, OnInit, Pipe, ElementRef, HostListener } from '@angular/core';
 import {STEPPER_GLOBAL_OPTIONS} from '@angular/cdk/stepper';
-
+import {VERSION} from '@angular/material';
 @Component({
+  host: {
+    '(document:click)': 'onClick($event)',
+  },
   selector: 'app-my-order',
   templateUrl: './my-order.component.html',
   styleUrls: ['./my-order.component.scss'],
@@ -10,7 +13,7 @@ import {STEPPER_GLOBAL_OPTIONS} from '@angular/cdk/stepper';
   }]
 })
 export class MyOrderComponent implements OnInit {
-  lenses=["-2.00","-2.25","-2.75","+2.00","+2.25","+2.75",'3.00','3.25',"-3.75","+3.00","+3.25","+3.75",'4.00','-4.25',"-4.75","+4.00","+4.25","+4.75", "-5.00"]
+ 
   jsObject = {
     '2': ["-2.00","-2.25","-2.75","+2.00","+2.25","+2.75"],
     '3': ["-3.00","-3.25","-3.75","+3.00","+3.25","+3.75"],
@@ -22,6 +25,12 @@ export class MyOrderComponent implements OnInit {
     '9': ["-9.00","-9.25","-9.75","+9.00","+9.25","+9.75"],
     '10': ["-10.00","-10.25","-10.75","+10.00","+10.25","+10.75"],
     '11': ["-11.00","-11.25","-11.75","+11.00","+11.25","+11.75"],
+    '12': ["-12.00","-12.25","-12.75","+12.00","+12.25","+12.75"],
+    '13': ["-13.00","-13.25","-13.75","+13.00","+13.25","+13.75"],
+    '14': ["-14.00","-14.25","-14.75","+14.00","+14.25","+14.75"],
+    '15': ["-15.00","-15.25","-15.75","+15.00","+15.25","+15.75"],
+    '16': ["-16.00","-16.25","-16.75","+16.00","+16.25","+16.75"],
+    '17': ["-17.00","-17.25","-17.75","+17.00","+17.25","+17.75"],
 };
   searchlense="+2.00"
   searchlense1
@@ -29,18 +38,27 @@ export class MyOrderComponent implements OnInit {
   numberfor=0
   newArray=[]
   value=[]
-  constructor() { }
-  transform(items: any[], field: string, value: string): any[] {
-    if (!items) return [];
-    return items.filter(it => it[field] == value);
+  str3=""
+  finished = false;
+  uncut= false;
+  both= false;
+  onlyLeft= false;
+  digital= false;
+  onlyRight= false;
+  version = VERSION;
+  public text: String;
+  constructor(private eRef: ElementRef) { }
+   @HostListener('document:click', ['$event'])
+  clickout(event) {
+    if(this.eRef.nativeElement.contains(event.target)) {
+      this.text = "clicked inside";
+    } else {
+      this.text = "clicked outside";
+    }
   }
+
   focusFunction() {
     document.getElementById("myForm").style.display = "block";
-   
-   
-   
-    
-   
   }
   focusOutFunction() {
     document.getElementById("myForm").style.display = "none";
@@ -48,11 +66,9 @@ export class MyOrderComponent implements OnInit {
   }
   arrayFor(){
     console.log(this.searchlense.length);
-   
       if(this.searchlense.length==6){
         this.searchlense1=this.searchlense.slice(1,3)
-        console.log(this.searchlense1);
-        
+        console.log(this.searchlense1); 
     }
     
    if(this.searchlense.length>1 && (this.searchlense.charAt(0)=='-') || (this.searchlense.charAt(0)=='+')){
@@ -61,6 +77,14 @@ export class MyOrderComponent implements OnInit {
    if(this.searchlense.length==1){
     this.searchlense1=this.searchlense.charAt(0)
    }
+   if(this.searchlense.length==3){
+    this.searchlense1=this.searchlense.slice(1,3)
+   }else if(this.searchlense.length==3){
+    this.searchlense1=this.searchlense.slice(1,3)
+   }
+   if(this.searchlense.length==2){
+    this.searchlense1=this.searchlense.slice(0,2)
+   }
     var item;
     if( this.jsObject.hasOwnProperty(this.searchlense1) ) {
         item = this.jsObject[this.searchlense1];
@@ -68,23 +92,60 @@ export class MyOrderComponent implements OnInit {
         console.log( this.value);
     }
   }
-  number(number){
-    console.log(number);
-    var str1="+"
-    var str2="-"
-    var str3=str1.concat(str2);
-    console.log(str3);
-    
-    if(number=='+'){
-      this.searchlense=number
-      this.numberfor=(this.numberfor+number)*10/10
-      this.searchlense=this.searchlense.concat(this.numberfor.toString())
+  jobType(type){
+    if(type=='finished'){
+      this.finished = true;
+    }else{
+      this.finished = false;
+      
     }
-    // this.numberfor=(this.numberfor+number)*10/10
-    // this.searchlense=this.numberfor.toString()
-    // console.log(this.numberfor);
+    if(type=='uncut'){
+      this.uncut = true;
+      
+    }else{
+      this.uncut = false;
+    }
+    if(type=='both'){
+      this.both = true;
+    }else{
+      this.both = false;
+    }
+    if(type=='onlyLeft'){
+      this.onlyLeft = true;
+    }else{
+      this.onlyLeft = false;
+    }
+    if(type=='onlyRight'){
+      this.onlyRight = true;
+    }else{
+      this.onlyRight = false;
+    }
+    if(type=='digital'){
+      this.digital = true;
+    }else{
+      this.digital = false;
+    }
+   
+  }
+  number(number){
+    if(number=='clear'){
+      this.searchlense = this.searchlense.slice(0, -1);
+      console.log(this.searchlense)
+      this.arrayFor()
+    }
+    if(number!='clear'){
+   var str1=number
+   console.log(str1);
+     this.searchlense=this.searchlense.concat(str1)
+    console.log(this.str3);
+    this.arrayFor()
+    }
     
       
+  }
+  clear(){
+    this.searchlense = this.searchlense.slice(0, -1);
+    console.log(this.searchlense)
   }
   ngOnInit() 
   {
